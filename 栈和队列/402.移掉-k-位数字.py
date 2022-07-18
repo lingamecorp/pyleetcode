ae5adf79dba05c88bff7fca85cc0b5df411fb531
lingame <lingamecorp@gmail.com>
2022-07-18 17:41:44 +0800

    402-移掉k位数字, Accepted; 主要思路还是维护一个单调递增栈, 不过要注意出栈的次数少于k和最终结果包含前置0或者最终栈为空等情况.

---
+++ 栈和队列/402.移掉-k-位数字.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode.cn id=402 lang=python3
+#
+# [402] 移掉 K 位数字
+#
+# https://leetcode.cn/problems/remove-k-digits/description/
+#
+# algorithms
+# Medium (32.19%)
+# Likes:    842
+# Dislikes: 0
+# Total Accepted:    112.2K
+# Total Submissions: 348.5K
+# Testcase Example:  '"1432219"\n3'
+#
+# 给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
+# 
+# 
+# 示例 1 ：
+# 
+# 
+# 输入：num = "1432219", k = 3
+# 输出："1219"
+# 解释：移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219 。
+# 
+# 
+# 示例 2 ：
+# 
+# 
+# 输入：num = "10200", k = 1
+# 输出："200"
+# 解释：移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+# 
+# 
+# 示例 3 ：
+# 
+# 
+# 输入：num = "10", k = 2
+# 输出："0"
+# 解释：从原数字移除所有的数字，剩余为空就是 0 。
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 
+# num 仅由若干位数字（0 - 9）组成
+# 除了 0 本身之外，num 不含任何前导零
+# 
+# 
+#
+
+# @lc code=start
+from collections import deque
+
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = deque([num[0]])  # 单调递增栈
+        count = 0
+        for i in range(1, len(num)):
+            while stack and count < k and num[i] < stack[-1]:
+                stack.pop()
+                count += 1
+            stack.append(num[i])
+        while stack and count < k:
+            stack.pop()
+            count += 1
+        while stack and stack[0] == '0':
+            stack.popleft()
+        if not stack:
+            return '0'
+        else:
+            return "".join(str(c) for c in stack)
+# @lc code=end
+
